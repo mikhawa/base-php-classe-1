8384d97e337ddf9e8563da659642ab435cf208cb
TNK-Sempaï <alioussalah.meidhy@gmail.com>
2026-03-16 09:54:17 +0100

    Exo 15

--- Stagiaire/Meidhy/15-conditions.php
+++ Stagiaire/Meidhy/15-conditions.php
@@ -3,9 +3,9 @@ $note = mt_rand(0, 10);
 
 if ($note <= 3) {
     echo "{$note} : Nul, étudie la prochaine fois";
-} elseif ($note >= 3 && $note <= 5 ){
+} elseif ($note <= 5 ){
     echo "{$note} : Peut mieux faire";
-} elseif ($note >= 6 && $note <= 7 ){
+} elseif ($note <= 7 ){
     echo "{$note} : Bien";
 } else {
     echo "{$note} : Très Bien";
